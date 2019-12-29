aa13a1da8aa0827ecd425323c14da2e38a9981ee
daisy <1808025154@qq.com>
2019-12-29 21:39:21 +0800

    项目初始化

---
+++ src/main/java/com/gmyx/website/config/dispatcher/DefaultView.java
@@ -0,0 +1,17 @@
+package com.gmyx.website.config.dispatcher;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class DefaultView extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
+    }
+}
